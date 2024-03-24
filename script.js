593d0aea9210f85db72e7607e76f25e48cdfdb65
HerbsMagic <136325970+HerbsMagic@users.noreply.github.com>
2024-03-24 11:58:58 +0530

    Script Changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       var currentTab = tabs[0];
       var tabTitle = currentTab.title;
-      document.getElementById('titleDisplay').innerText = tabTitle;
+      document.getElementById('titleDisplay').innerHTML = "<h3>Title of Current Tab:</h3>" + `<h3>${tabTitle}</h3>`;
     });
   });
   
